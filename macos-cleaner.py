85050b6ee20a5a5f08b6b4db420d43659d2a5d87
Yiheng (NUC) <development.henryquan@gmail.com>
2020-02-09 15:59:16 +1100

    working

--- macos-cleaner.py
+++ macos-cleaner.py
@@ -4,14 +4,30 @@ import re, glob
 max_depth = 5
 
 def usage():
-    print('usage: python3 macos-cleaner <path>\nmacos-cleaner will remove all .DS_Store and ._* files for all subdirectories')
+    print('usage: python3 macos-cleaner <path> [-y|--yes]\nmacos-cleaner will remove all .DS_Store and ._* files for all subdirectories')
 
 def main():
     length: int = len(sys.argv)
     if length > 1:
         path = sys.argv[1]
-        count = clean(path, 0)
-        print('{} file(s) cleaned'.format(count))
+        if length > 2:
+            # no warning
+            confirmation = sys.argv[2]
+            if confirmation == '-y' or confirmation == '--yes':
+                count = clean(path, 0)
+                print('{} file(s) cleaned'.format(count))
+                exit(0)
+        else:
+            # warn users first
+            first_confirmation = input('This program will remove all .DS_Store files and every single file that starts with ._\nThis operation cannot be reverted. Are you sure you want to continue? (y/n) ').lower()
+            if first_confirmation == 'y' or first_confirmation == 'yes':
+                second_confirmation = input('Are you really really sure you want to continue? (y/n) ').lower()
+                if second_confirmation == 'y' or first_confirmation == 'yes':
+                    count = clean(path, 0)
+                    print('{} file(s) cleaned'.format(count))
+                    exit(0)
+
+        print('The operation was cancelled')
     else:
         usage()
 
@@ -21,14 +37,22 @@ def clean(path: str, depth: int):
         return curr_count
 
     if os.path.exists(path):
-        for f in glob.glob(os.path.join(path, '*.*')):
-            regex = re.compile('\._.*$')
-            print(f, depth)
-            if f.endswith('.DS_Store') or regex.search(f):
-                print(f)
+        directory = os.path.join(path, '*')
+        temp_file = os.path.join(path, '.*')
 
-            if os.path.isdir(f):
-                curr_count += clean(f, depth + 1)
+        # find files to remove
+        for f in glob.glob(temp_file):
+            regex = re.compile('\._.*$')
+            if f.endswith('.DS_Store') or regex.search(f):
+                os.remove(f)
+                print('{} was removed'.format(f))
+                curr_count += 1
+        
+        # go deeper
+        for d in glob.glob(directory):
+            if os.path.isdir(d):
+                new_path = os.path.join(path, d)
+                curr_count += clean(new_path, depth + 1)
                 
         return curr_count
     else:
